be73d2d9b7d0ba7b61dad541e7b55d631267160e
Shanae Chu <father@shabae.com>
2024-03-23 13:41:15 -0700

    phps

---
+++ chu.shanae/shop.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Shop</title>
+		<?php include "parts/meta.php"; ?>
+</head>
+<body>
+
+
+<?php include "parts/navbar.php"; ?>
+
+
+<div class="container">
+	<div class="card soft">
+		
+
+		<ul>
+			<li><a href="product_item.php?id=1" class="">Product 1</a></li>
+			<li><a href="product_item.php?id=2" class="">Product 2</a></li>
+			<li><a href="product_item.php?id=3" class="">Product 3</a></li>
+			<li><a href="product_item.php?id=4" class="">Product 4</a></li>
+		</ul>
+	</div>
+</div>
+	
+</body>
+</html>
