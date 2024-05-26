3cecad98adac9ecc6eab55fd1f96f7e1c6c9d311
Shanae Chu <father@shabae.com>
2024-05-26 15:41:02 -0700

    final

--- chu.shanae/parts/meta.php
+++ chu.shanae/parts/meta.php
@@ -1,10 +1,22 @@
-	<meta name="viewport" content="width=device-width">
-    
 
-    <base href="https://shabae.com/aau/wnm608/chu.shanae/">
-    
-   <link rel="stylesheet" href="lib/css/styleguide.css">
-   <link rel="stylesheet" href="lib/css/gridsystem.css">
-   <link rel="stylesheet" href="css/storetheme.css">
+	<meta charset="UTF-8">
+	<base href="https://shabae.com/aau/wnm608/chu.shanae/">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="lib/css/styleguide.css">
+	<link rel="stylesheet" href="lib/css/gridsystem.css">
+	<link rel="stylesheet" href="css/storetheme.css">
+	
+    <!----script--->
+	<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+	<script src="lib/js/functions.js"></script>
+	<script src="js/templates.js"></script>
+	<script src="js/product_list.js"></script>
 
-   <script src="https://code.jquery.jquery-3.2.1.min.js"></script>
+	
+	<!----- fonts ----->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet">
+
+	
+ 
--- chu.shanae/parts/navbar.php
+++ chu.shanae/parts/navbar.php
@@ -1,25 +1,30 @@
 <?php 
 
 include_once "lib/php/functions.php";
-
 ?>
-<header class="navbar">
-    </div>
 
+<input type="checkbox" id="menu" class="hidden">
+<header class=" navbar" >
 		<div class="container display-flex">
 			<div class="flex-none">
-			<div class="styletitle">Sonder</div>
+				<h1><a href="index.php">Sonder</a></h1>
 			</div>
 			<div class="flex-stretch"></div>
-			<nav class="nav nav-pills ">
-				<ul>
+			<div class="flex-none menu-button">
+				<label for="menu">&equiv;</label>
+			</div>
+			<nav class="nav nav-pills">
+				<ul class="container display-flex">
 					<li><a href="index.php">Home</a></li>
-					<li><a href="product_list.php">Store</a></li>
+					<li><a href="product_list.php">Shop</a></li>
+					<li><a href="about.php">About Us</a></li>
+
 					<li><a href="product_cart.php">
 						<span>Cart</span>
-						<span class="badge"><?= makeCartBadge(); ?></span>
-				</a></ul>
+						<span class="badge"><?= makeCartBadge() ?></span>
+
+					</a></li>
+				</ul>
 			</nav>
-			</div>
 		</div>
-	</header>
+</header>
