3c2e9edeae929daf41c8fca9786d64b1cd244783
Shanae Chu <father@shabae.com>
2024-05-26 15:39:08 -0700

    final

--- chu.shanae/lib/php/functions.php
+++ chu.shanae/lib/php/functions.php
@@ -1,95 +1,96 @@
 <?php
-function print_p($v) {
-    echo "<pre>",print_r($v),"</pre>";
-}
 
-function file_get_json($filename) {
-    $file = file_get_contents($filename);
-    return json_decode($file);
-}
+session_start();
 
-include_once "auth.php";
-function makeConn() {
-    $conn = new mysqli(...MYSQLIAuth());
-    if($conn->connect_errno) die($conn->connect_error);
-    $conn->set_charset('utf8');
-    return $conn;
-}
+    function print_p($v) {
+        echo "<pre>",print_r($v),"</pre>";
+    }
 
-function makeQuery($conn,$qry) {
-    $result = $conn->query($qry);
-    if($conn->errno) die($conn->error);
-    $a = [];
-    while($row = $result->fetch_object()) {
-        $a[] = $row;
-    }  
-    return $a;  
-}
 
-$sql = "SELECT * FROM `products`;";
+    function file_get_json($filename){
+        $file = file_get_contents($filename);
+        return json_decode($file);
+    }
 
-//cartfunctions 
+    include_once "auth.php";
 
-function array_find($array,$fn) {
+    function makeConn(){
+        $conn = new mysqli(...MYSQLIAuth());// ... is a spread operator, making an array into values
+        if($conn->connect_errno) die($conn->connect_error);//errno error number, error actual error. if there is an error, show the error.
+        // $conn->set_charset('utf-8');
+        return $conn;
+    }
+
+    function PDOConn(){
+        try {
+            $conn = new PDO(...PDOAuth());
+        } catch(PDOException $e){
+            die($e->getMessage());
+        }
+        return $conn;
+    }
+
+    function makeQuery($conn,$qry){
+        $result = $conn->query($qry);
+        if($conn->errno) die($conn->error);
+        $a = [];
+        while($row = $result->fetch_object()){
+            $a[] = $row;
+        }
+        return $a;
+    }
+
+function array_find($array,$fn){
     foreach($array as $o) if($fn($o)) return $o;
     return false;
 }
 
-function getCart() {
-    return isset($_SESSION['cart']) ? $_SESSION['cart'] : [];
+function getCart(){
+    return isset($_SESSION['cart']) ? $_SESSION['cart'] :[];
 }
 
-function addToCart($id, $amount) {
+function addToCart($id,$amount,$color){
     $cart = getCart();
+    $p = array_find($cart,function($o) use($id){return $o->id==$id;});
 
-    // Check if the cart is empty or if the product doesn't exist in the cart
-    if (empty($cart) || !cartItemById($id)) {
-        $_SESSION['cart'][] = (object)[
-            "id" => $id,
-            "amount" => $amount
-        ];
+    if($p){
+        $p->amount += $amount; 
     } else {
-        // If the product already exists in the cart, update its quantity
-        foreach ($cart as $item) {
-            if ($product->id == $id) {
-                $product->amount += $amount;
-            }
+        $_SESSION['cart'][] = (object) [
+            "id"=>$id,
+            "amount"=>$amount,
+            "color"=>$color
+        ];
+        
         }
-        $_SESSION['cart'] = $cart; // Update the session cart with the modified cart
-    }
 }
 
-function resetCart() { $_SESSION['cart'] = []; }
+function resetCart(){ $_SESSION['cart'] = []; }
 
-function cartItemById($id) {
-    return array_find(getCart(),function($o)use($id){return $o->id==$id;});
-}
-
-function makeCartBadge() {
+function makeCartBadge(){
     $cart = getCart();
     if(count($cart)==0) {
         return "";
-    } else {
-        return "(".array_reduce($cart,function($r,$o){return $r+$o->amount;},0).")";
-
+    }else{
+        return array_reduce($cart,function($r,$o) {return $r+$o->amount;},0);
     }
 }
 
-function getCartItems() {
+function getCartItems(){
     $cart = getCart();
-
     if(empty($cart)) return [];
 
     $ids = implode(",",array_map(function($o){return $o->id;},$cart));
-    $data = makeQuery(makeConn(),"SELECT * FROM `products` WHERE `id` IN ($ids)");
-
-   return  array_map(function($o) use ($cart) {
+    $data = makeQuery(makeConn(),"SELECT *FROM `products` WHERE `id` IN ($ids)");
+    return array_map(function($o) use($cart){
         $p = cartItemById($o->id);
         $o->amount = $p->amount;
         $o->total = $p->amount * $o->price;
         return $o;
-
-   },$data);
+    },$data);
 }
 
+function cartItemById($id){
+    return array_find(getCart(),function($o) use($id) { return $o->id==$id;});
+}
 ?>
